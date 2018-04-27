f5246d14af0746d9b68073dd8c1e131631353cbc
Justin Revenaugh <justinr@umn.edu>
2018-04-26 21:14:52 -0500

    New code.

---
+++ procETOPO.R
@@ -0,0 +1,30 @@
+# Create ETOPO5.RDS file for use with bathos.
+
+# Read ETOPO5.DAT big-endian two-byte signed integer binary
+# Available from:
+# https://www.ngdc.noaa.gov/mgg/global/relief/ETOPO5/TOPO/ETOPO5/
+
+etopoIn <- "ETOPO5.DAT"         # Path to data
+m <- readBin(etopoIn,
+             "int",
+             endian = "big",
+             size = 2,
+             signed = TRUE,
+             n = (360 * 12 * 180 * 12))
+
+# Convert to matrix and shift rows so it goes from -179.9167 to 180
+M <- matrix(m, nrow = 4320, byrow = FALSE)
+M <- rbind(M[2161:4320,], M[1:2160,])
+
+# Create lon, lat sequences
+lon <- seq(-180 + 1 / 12, by = 1 / 12, length.out = 4320)
+lat <- seq(90, -90, -1 / 12)
+
+# Downloaded data skips the south pole (elevation 2810 m), so let's add it
+# (lat sequence already includes it).
+M <- cbind( M, rep( 2810, 4320))
+
+etopo5_obj <- list(x = lon, y = lat, z = M)
+
+etopoOut <- "ETOPO5_1.RDS"       # Output path
+saveRDS(object = etopo5_obj, file = etopoOut )
